3a60f3a28341ec26615aed98045d285c036cc152
Yogi Sukmawati <76022371+yogisukma25@users.noreply.github.com>
2023-08-02 15:00:33 +0700

    latihan_1

---
+++ latihan/app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $user = User::where('id', 1)->first();
+        return view('home', compact('user'));
+    }
+}
